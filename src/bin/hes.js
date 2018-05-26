913526a4721e7f490a1c08965f969cd2d8860457
Travis Clarke <travis.m.clarke@gmail.com>
2018-05-26 16:11:40 -0700

    v1.0.4

--- src/bin/hes.js
+++ src/bin/hes.js
@@ -5,11 +5,11 @@ import commander from 'commander';
 
 commander
   .version('1.0.1')
-  .option('-i, --input path', 'Input path.', 'content/**')
-  .option('-o, --output path', 'Output path.', 'public/elasticsearch.json')
-  .option('-l, --language lang', 'Language [toml | yaml].', 'toml')
-  .option('-d, --delimiter delim', 'Delimiter [toml: +++ | yaml: ---].  (optional)')
-  .option('-n, --index-name name', 'Index name. (optional)')
+  .option('-i, --input [path]', 'Input path.', 'content/**')
+  .option('-o, --output [path]', 'Output path.', 'public/elasticsearch.json')
+  .option('-l, --language [lang]', 'Language [toml | yaml].', 'toml')
+  .option('-d, --delimiter [delim]', 'Delimiter [toml: +++ | yaml: ---].  (optional)')
+  .option('-n, --index-name [name]', 'Index name. (optional)')
   .parse(process.argv);
 
 const {input, output, language, delimiter, indexName} = commander;
