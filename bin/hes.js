364756e253c60ab3c7a6905311a83b3853358eb2
Travis Clarke <travis.m.clarke@gmail.com>
2018-08-30 22:41:57 -0700

    rebuild

--- bin/hes.js
+++ bin/hes.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-var _ = require('../');
+var _ = require("../");
 
 var _2 = _interopRequireDefault(_);
 
-var _commander = require('commander');
+var _commander = require("commander");
 
 var _commander2 = _interopRequireDefault(_commander);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _require = require('../package.json'),
+var _require = require("../package.json"),
     version = _require.version;
 
-_commander2.default.version(version).option('-i, --input [path]', 'Input path.', 'content/**').option('-o, --output [path]', 'Output path.', 'public/elasticsearch.json').option('-l, --language [lang]', 'Language [toml | yaml].', 'toml').option('-d, --delimiter [delim]', 'Delimiter [toml: +++ | yaml: ---].  (optional)').option('-n, --index-name [name]', 'Index name. (optional)').parse(process.argv);
+_commander2.default.version(version).option("-i, --input <path>", "Input path.", "content/**").option("-o, --output <path>", "Output path.", "public/elasticsearch.json").option("-l, --language <lang>", "Language [toml | yaml].", "toml").option("-d, --delimiter <delim>", "Delimiter [toml: +++ | yaml: ---].  (optional)").option("-n, --index-name <name>", "Index name. (optional)").parse(process.argv);
 
 var input = _commander2.default.input,
     output = _commander2.default.output,
