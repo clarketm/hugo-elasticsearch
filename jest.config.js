14d5e7bbdd2baa584d7e3052d7c90a9622bc76e5
Travis Clarke <travis.m.clarke@gmail.com>
2018-08-30 22:39:06 -0700

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  collectCoverageFrom: ["lib/**/*.{js,jsx,mjs}"],
+  testMatch: ["<rootDir>/**/?(*)(spec|test).{js,jsx,mjs}"],
+  testEnvironment: "node",
+  moduleFileExtensions: ["web.js", "mjs", "js", "json", "web.jsx", "jsx", "node"]
+};
