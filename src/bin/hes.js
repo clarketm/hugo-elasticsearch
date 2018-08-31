287ea08a13e91c65274c24e999184bc265d0fc55
Travis Clarke <travis.m.clarke@gmail.com>
2018-08-30 22:41:27 -0700

    demark position parameters as required

--- src/bin/hes.js
+++ src/bin/hes.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
-import hes from '../';
-const { version } = require('../package.json');
-import commander from 'commander';
+import hes from "../";
+const { version } = require("../package.json");
+import commander from "commander";
 
 commander
   .version(version)
-  .option('-i, --input [path]', 'Input path.', 'content/**')
-  .option('-o, --output [path]', 'Output path.', 'public/elasticsearch.json')
-  .option('-l, --language [lang]', 'Language [toml | yaml].', 'toml')
-  .option('-d, --delimiter [delim]', 'Delimiter [toml: +++ | yaml: ---].  (optional)')
-  .option('-n, --index-name [name]', 'Index name. (optional)')
+  .option("-i, --input <path>", "Input path.", "content/**")
+  .option("-o, --output <path>", "Output path.", "public/elasticsearch.json")
+  .option("-l, --language <lang>", "Language [toml | yaml].", "toml")
+  .option("-d, --delimiter <delim>", "Delimiter [toml: +++ | yaml: ---].  (optional)")
+  .option("-n, --index-name <name>", "Index name. (optional)")
   .parse(process.argv);
 
-const {input, output, language, delimiter, indexName} = commander;
+const { input, output, language, delimiter, indexName } = commander;
 
-new hes({input, output, language, delimiter, indexName}).index();
-
+new hes({ input, output, language, delimiter, indexName }).index();
