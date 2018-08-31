7e94d6d1d9b4e92e281fe334e180a2af6b7b8631
Travis Clarke <travis.m.clarke@gmail.com>
2018-08-30 22:41:51 -0700

    minor formatting and context persistence updates

--- src/lib/hes.js
+++ src/lib/hes.js
@@ -1,106 +1,106 @@
-import fs from 'fs';
-import glob from 'glob';
-import matter from 'gray-matter';
-import toml from 'toml';
-import removeMd from 'remove-markdown';
-import striptags from 'striptags';
-import path from 'path';
-import minimalist from 'minimist';
+import fs from "fs";
+import glob from "glob";
+import matter from "gray-matter";
+import toml from "toml";
+import removeMd from "remove-markdown";
+import striptags from "striptags";
+import path from "path";
+import minimalist from "minimist";
 
 class HugoElasticsearch {
-
   ////////////////////////////////////////////
   // Constructor
   ////////////////////////////////////////////
 
-  constructor (config = {}) {
+  constructor(config = {}) {
     const argv = minimalist(process.argv.slice(2));
 
-    this.indexMeta = {index: {}};
+    this.indexMeta = { index: {} };
 
     // DEFAULTS
-    this.input = config.input || 'content/**';
-    this.output = config.output || 'public/elasticsearch.json';
-    this.language = config.language || 'toml';
+    this.input = config.input || "content/**";
+    this.output = config.output || "public/elasticsearch.json";
+    this.language = config.language || "toml";
     this.delimiter = config.delimiter;
     this.indexName = config.indexName; // 'posts';
 
     // Input
-    if (argv['i']) this.setInput(argv['i']);
-    if (argv['input']) this.setInput(argv['input']);
+    if (argv["i"]) this.setInput(argv["i"]);
+    if (argv["input"]) this.setInput(argv["input"]);
 
     // Output
-    if (argv['o']) this.setOutput(argv['o']);
-    if (argv['output']) this.setOutput(argv['output']);
+    if (argv["o"]) this.setOutput(argv["o"]);
+    if (argv["output"]) this.setOutput(argv["output"]);
 
     // Language
-    if (argv['l']) this.setLanguage(argv['l']);
-    if (argv['language']) this.setLanguage(argv['language']);
+    if (argv["l"]) this.setLanguage(argv["l"]);
+    if (argv["language"]) this.setLanguage(argv["language"]);
 
     // Delimiter
-    if (argv['d']) this.setDelimiter(argv['d']);
-    if (argv['delimiter']) this.setDelimiter(argv['delimiter']);
+    if (argv["d"]) this.setDelimiter(argv["d"]);
+    if (argv["delimiter"]) this.setDelimiter(argv["delimiter"]);
 
     // Index Name
-    if (argv['n']) this.setIndexName(argv['n']);
-    if (argv['name']) this.setIndexName(argv['name']);
-
+    if (argv["n"]) this.setIndexName(argv["n"]);
+    if (argv["name"]) this.setIndexName(argv["name"]);
   }
 
   ////////////////////////////////////////////
   // Setters
   ////////////////////////////////////////////
 
-  setInput (input) {
+  setInput(input) {
     this.input = input;
   }
 
-  setOutput (output) {
+  setOutput(output) {
     this.output = output;
   }
 
-  setLanguage (language) {
+  setLanguage(language) {
     this.language = language;
   }
 
-  setDelimiter (delimiter) {
+  setDelimiter(delimiter) {
     this.delimiter = delimiter;
   }
 
-  setIndexName (indexName) {
+  setIndexName(indexName) {
     this.indexName = indexName;
   }
 
-  setLanguageConfig (language, delimiter) {
+  setLanguageConfig(language, delimiter) {
     switch (true) {
-      case language.toLowerCase() === 'yaml':
+      case language.toLowerCase() === "yaml":
+        this.delimiter = delimiter || "---";
         this.languageConfig = {
-          delims: delimiter || '---',
-          lang: 'yaml'
+          delims: this.delimiter,
+          lang: "yaml"
         };
         break;
       default:
-      case language.toLowerCase() === 'toml':
+      case language.toLowerCase() === "toml":
+        this.delimiter = delimiter || "+++";
         this.languageConfig = {
-          delims: delimiter || '+++',
-          lang: 'toml',
+          delims: this.delimiter,
+          lang: "toml",
           engines: {
-            toml: toml.parse.bind(toml),
+            toml: toml.parse.bind(toml)
           }
         };
         break;
     }
   }
 
-  setIndexMetaId (id) {
+  setIndexMetaId(id) {
     this.indexMeta.index._id = id || this.id++;
   }
 
-  setIndexMetaIndex (indexName) {
+  setIndexMetaIndex(indexName) {
     this.indexMeta.index._index = indexName;
   }
 
-  writeIndexStream (input, output, indexMeta) {
+  writeIndexStream(input, output, indexMeta) {
     this.id = 1;
     this.list = [];
     this.baseDir = path.dirname(this.input);
@@ -114,11 +114,11 @@ class HugoElasticsearch {
     for (let i = 0; i < this.list.length; i++) {
       this.setIndexMetaId();
       this.stream.write(JSON.stringify(indexMeta));
-      this.stream.write('\n');
+      this.stream.write("\n");
       this.stream.write(JSON.stringify(this.list[i]));
-      this.stream.write('\n');
+      this.stream.write("\n");
     }
-    this.stream.write('\n');
+    this.stream.write("\n");
     this.stream.end();
   }
 
@@ -126,12 +126,12 @@ class HugoElasticsearch {
   // Methods
   ////////////////////////////////////////////
 
-  createOutputDirectory (output) {
+  createOutputDirectory(output) {
     const dir = path.dirname(output);
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
   }
 
-  readInputDirectory (path) {
+  readInputDirectory(path) {
     const files = glob.sync(path);
 
     for (let file of files) {
@@ -140,7 +140,7 @@ class HugoElasticsearch {
     }
   }
 
-  readInputFile (filePath) {
+  readInputFile(filePath) {
     const ext = path.extname(filePath);
     const meta = matter.read(filePath, this.languageConfig);
 
@@ -151,23 +151,22 @@ class HugoElasticsearch {
     let content;
 
     // Content
-    if (ext === '.md') content = removeMd(meta.content);
+    if (ext === ".md") content = removeMd(meta.content);
     else content = striptags(meta.content);
 
     // Uri
-    let uri = `/${filePath.substring(0, filePath.lastIndexOf('.'))}`.replace(`${this.baseDir}/`, '');
+    let uri = `/${filePath.substring(0, filePath.lastIndexOf("."))}`.replace(`${this.baseDir}/`, "");
 
     if (meta.data.slug) uri = path.dirname(uri) + meta.data.slug;
 
     if (meta.data.url) uri = meta.data.url;
 
-    this.list.push({...meta.data, uri, content, tags});
+    this.list.push({ ...meta.data, uri, content, tags });
   }
 
-  index () {
+  index() {
     this.writeIndexStream(this.input, this.output, this.indexMeta);
   }
-
 }
 
 module.exports = HugoElasticsearch;
