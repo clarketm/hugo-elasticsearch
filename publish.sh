2091c0d90ffbe3eef4634e4b4a4cfbf89b2eef7f
Travis Clarke <travis.m.clarke@gmail.com>
2018-10-25 23:55:18 -0700

    update publish.sh with --quiet

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if git diff --exit-code -- . ':(exclude)package.json'; then
+if git diff --exit-code --quiet -- . ':(exclude)package.json'; then
   echo 'Clean working tree'
 else
   echo 'Dirty working tree'
